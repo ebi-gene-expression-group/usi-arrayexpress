70654f3df580e14cc380a0afc8c22a6ac5583e5e
Anja Fullgrabe <anjaf@ebi.ac.uk>
2019-02-14 10:40:06 +0000

    Add executable for metadata valiation

--- magetab_validation.py
+++ magetab_validation.py
@@ -1,13 +1,16 @@
 """
-This script takes an IDF file as input and runs metadata conversion from MAGE-TAB to USI-JSON format.
+This script takes an IDF file as input and runs validation of the metadata in the common datamodel.
 """
 
 import argparse
 import os
+import logging
 
 from utils.common_utils import create_logger
 from utils.converter_utils import get_sdrf_path
-from converter.converting import data_objects_from_magetab, datamodel2json_conversion
+from converter.converting import data_objects_from_magetab
+
+import validator.metadata_validation as mv
 
 
 def parse_args():
@@ -21,13 +24,16 @@ def parse_args():
 
 
 def main():
-    process_name = "mtab2usi_conversion"
+    process_name = "magetab_validation"
 
     idf_file = parse_args()
 
     # Create logger
     current_dir, idf_file_name = os.path.split(idf_file)
     logger = create_logger(current_dir, process_name, idf_file_name)
+    # Add handler to direct output to screen in addition to log file
+    stream_hdlr = logging.StreamHandler()
+    logger.addHandler(stream_hdlr)
 
     # Get path to SDRF file
     sdrf_file_path = get_sdrf_path(idf_file, logger)
@@ -35,8 +41,11 @@ def main():
     # Read in MAGE-TAB and convert to common data model
     sub = data_objects_from_magetab(idf_file, sdrf_file_path)
 
-    # Dump data in common data model as USI-JSON files
-    datamodel2json_conversion(sub, current_dir, logger)
+    # Validate metadata in common data model
+    mv.run_project_checks(sub, logger)
+    mv.run_study_checks(sub, logger)
+    mv.run_protocol_checks(sub, logger)
+    mv.run_sample_checks(sub, logger)
 
 
 if __name__ == '__main__':
