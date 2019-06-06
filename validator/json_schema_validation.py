5742abdcb2f9c077597f7e7502cc53a0cf12f0e2
Anja Fullgrabe <anjaf@ebi.ac.uk>
2019-06-06 14:22:49 +0100

    Make AE submission schema default with option to pass in external schema file

--- validator/json_schema_validation.py
+++ validator/json_schema_validation.py
@@ -7,20 +7,17 @@ import json_schemas
 from utils.converter_utils import read_json_file
 
 
-def load_submission_schema():
-    """Find and load the ArrayExpress submission schema as JSON"""
+def validate_submission_json(json_file, schema_file=None):
+    """Match a JSON object against a JSON schema and return a human-readable list of all validation errors."""
 
-    # Construct path to schema and load as json
-    schema_path = os.path.dirname(json_schemas.__file__)
-    schema_file = os.path.join(schema_path, "arrayexpress_submission.json")
-    schema = read_json_file(schema_file)
+    json_data = read_json_file(json_file)
 
-    return schema_file, schema
-
-
-def validate_submission_json(submission_json):
-
-    schema_file, schema = load_submission_schema()
+    if not schema_file:
+        # If no other schema is given, load the schema describing a full ArrayExpress submission
+        schema_file, schema = load_arrayexpress_submission_schema()
+    else:
+        # Parse the schema_file
+        schema = read_json_file(schema_file)
 
     # Create validator with resolver to help locate the referenced submittable schemas with absolute path
     resolver = jsonschema.RefResolver("file://" + schema_file, schema)
@@ -28,30 +25,25 @@ def validate_submission_json(submission_json):
 
     # Check validity of schema first
     # We shouldn't need this but for now, running this test before calling the checks on the object
-    validator.check_schema(schema)
+    schema_errors = validator.check_schema(schema)
 
     # Validate the submission JSON against the submission schema
-    errors = validator.iter_errors(submission_json)
+    validation_errors = validator.iter_errors(json_data)
 
     # Print out all error messages with where and why details
-    for e in errors:
+    for e in validation_errors:
         print(format_json_error_message(e))
 
 
-def recurse_through_errors(error_tree, traceback):
-    """The error tree arranges the error messages in the same structure as the validated instance.
-    This allows to get the traceback of which exact instance is failing validation."""
+def load_arrayexpress_submission_schema():
+    """Find and load the ArrayExpress submission schema as JSON"""
 
-    if error_tree.errors:
-        # Found a non-empty error dict, this is the lowest level
-        print(error_tree.errors)
-    else:
-        # Go a level deeper and look for errors
-        for branch in error_tree:
-            traceback.append(branch)
-            print(branch)
-            print(traceback)
-            recurse_through_errors(error_tree[branch], traceback)
+    # Construct path to schema and load as json
+    schema_path = os.path.dirname(json_schemas.__file__)
+    schema_file = os.path.join(schema_path, "arrayexpress_submission_schema.json")
+    schema = read_json_file(schema_file)
+
+    return schema_file, schema
 
 
 def format_traceback_path(path_element_list):
@@ -82,3 +74,17 @@ def format_json_error_message(error_object):
 
     return error_string
 
+
+# not used currently
+def recurse_through_errors(error_tree, traceback):
+    """The error tree arranges the error messages in the same structure as the validated instance.
+    This allows to get the traceback of which exact instance is failing validation."""
+
+    if error_tree.errors:
+        # Found a non-empty error dict, this is the lowest level
+        print(error_tree.errors)
+    else:
+        # Go a level deeper and look for errors
+        for branch in error_tree:
+            traceback.append(branch)
+            recurse_through_errors(error_tree[branch], traceback)
