59dd2431196af3345536a429881ab4f5ceea22d8
Anja Fullgrabe <anjaf@ebi.ac.uk>
2019-06-06 14:19:11 +0100

    Option to pass in external schema file

--- json2mtab_conversion.py
+++ json2mtab_conversion.py
@@ -9,6 +9,7 @@ import argparse
 import os
 
 from converter import json2dm
+from validator.json_schema_validation import validate_submission_json
 from utils.common_utils import file_exists, create_logger
 from utils.converter_utils import read_json_file
 
@@ -17,6 +18,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('json',
                         help="Path to the USI-JSON file")
+    parser.add_argument('-s', '--schema',
+                        help="Path to the JSON schema file")
     parser.add_argument('-v', '--verbose', action='store_const', const=10, default=20,
                         help="Option to output detailed logging (debug level).")
     args = parser.parse_args()
@@ -35,11 +38,10 @@ def main():
 
     #logger = create_logger(os.path.dirname(json_file), process_name, json_file)
 
-    json_data = read_json_file(json_file)
+    validate_submission_json(json_file)
 
-    sub = json2dm.data_objects_from_json(json_data, json_file)
-    print(sub.sample)
-
+    #sub = json2dm.data_objects_from_json(json_data, json_file)
+    #print(sub.sample)
 
 
 if __name__ == '__main__':
