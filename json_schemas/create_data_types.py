34ab1aaf1507dd7bfdd1b7626eaf5a8512e179df
Anja Fullgrabe <anjaf@ebi.ac.uk>
2019-11-14 16:37:58 +0000

    Data types for assays and protocols

--- json_schemas/create_data_types.py
+++ json_schemas/create_data_types.py
@@ -1,3 +1,11 @@
+"""A script to define the USI data types insert them into the database.
+
+Environment variables are needed to specify the following parameters:
+DBCON: MongoDB connection string
+SUBS_DATABASE: The database name to write to
+DATA_TYPE_COLLECTION: The name of the collection for storing the datatypes
+"""
+
 
 import json
 import re
@@ -11,8 +19,9 @@ def load_schema_for_mongo(schema_file):
         with open(schema_file) as sf:
             json_string = json.dumps(json.load(sf))
             print(json_string)
-            new_string = re.sub(r"\$", "#dollar#", json_string)
-            return json.loads(new_string)
+            mongo_string = re.sub(r"\"\$", "\"#dollar#", re.sub(r"\"\.", "\"#dot#", json_string))
+            print(mongo_string)
+            return json.loads(mongo_string)
     except IOError:
         print("Schema file {} not found or not readable.".format(schema_file))
 
@@ -49,10 +58,77 @@ new_data_types = {
                                   "optionalValidationAuthors": [],
                                   "submittableClassName": "uk.ac.ebi.subs.repository.model.Study",
                                   "archive": "ArrayExpress"},
-    "functionalGenomicsProtocols": {},
-    "functionalGenomicsMicroarrayAssays": {},
-    "functionalGenomicsSequencingAssays": {},
-    "functionalGenomicsSingleCellAssays": {},
+
+    "functionalGenomicsProtocols": {"displayNameSingular": "Functional Genomics Protocol",
+                                    "displayNamePlural": "Functional Genomics Protocols",
+                                    "description": "Protocols describing the steps of a functional genomics experiment",
+                                    "validationSchema": load_schema_for_mongo("arrayexpress_protocol_schema.json"),
+                                    "refRequirements": [],
+                                    "requiredValidationAuthors": ["core",
+                                                                  "JsonSchema"],
+                                    "optionalValidationAuthors": [],
+                                    "submittableClassName": "uk.ac.ebi.subs.repository.model.Protocol",
+                                    "archive": "ArrayExpress"},
+
+    "functionalGenomicsMicroarrayAssays": {"displayNameSingular": "Microarray Assay",
+                                           "displayNamePlural": "Microarray Assays",
+                                           "description": "",
+                                           "validationSchema": load_schema_for_mongo("arrayexpress_microarray_assay_schema.json"),
+                                           "refRequirements": [
+                                               {"refClassName": "uk.ac.ebi.subs.data.component.StudyRef",
+                                                "dataTypeIdForReferencedDocument": "functionalGenomicsStudies",
+                                                "additionalRequiredValidation": []},
+                                               {"refClassName": "uk.ac.ebi.subs.data.component.ProtocolUse",
+                                                "dataTypeIdForReferencedDocument": "functionalGenomicsProtocols",
+                                                "additionalRequiredValidation": []},
+                                               {"refClassName": "uk.ac.ebi.subs.data.component.SampleUse",
+                                                "dataTypeIdForReferencedDocument": "functionalGenomicsSamples",
+                                                "additionalRequiredValidation": []}],
+                                           "requiredValidationAuthors": ["core",
+                                                                         "JsonSchema"],
+                                           "optionalValidationAuthors": [],
+                                           "submittableClassName": "uk.ac.ebi.subs.repository.model.Assay",
+                                           "archive": "ArrayExpress"},
+
+    "functionalGenomicsSequencingAssays": {"displayNameSingular": "Functional Genomics Sequencing Assay",
+                                           "displayNamePlural": "Functional Genomics Sequencing Assays",
+                                           "description": "",
+                                           "validationSchema": load_schema_for_mongo("arrayexpress_sequencing_assay_schema.json"),
+                                           "refRequirements": [
+                                               {"refClassName": "uk.ac.ebi.subs.data.component.StudyRef",
+                                                "dataTypeIdForReferencedDocument": "functionalGenomicsStudies",
+                                                "additionalRequiredValidation": []},
+                                               {"refClassName": "uk.ac.ebi.subs.data.component.ProtocolUse",
+                                                "dataTypeIdForReferencedDocument": "functionalGenomicsProtocols",
+                                                "additionalRequiredValidation": []},
+                                               {"refClassName": "uk.ac.ebi.subs.data.component.SampleUse",
+                                                "dataTypeIdForReferencedDocument": "functionalGenomicsSamples",
+                                                "additionalRequiredValidation": []}],
+                                           "requiredValidationAuthors": ["core",
+                                                                         "JsonSchema"],
+                                           "optionalValidationAuthors": [],
+                                           "submittableClassName": "uk.ac.ebi.subs.repository.model.Assay",
+                                           "archive": "ArrayExpress"},
+
+    "functionalGenomicsSingleCellAssays": {"displayNameSingular": "Single Cell Sequencing Assay",
+                                           "displayNamePlural": "Single Cell Sequencing Assays",
+                                           "description": "",
+                                           "validationSchema": load_schema_for_mongo("arrayexpress_singlecell_assay_schema.json"),
+                                           "refRequirements": [
+                                               {"refClassName": "uk.ac.ebi.subs.data.component.StudyRef",
+                                                "dataTypeIdForReferencedDocument": "functionalGenomicsStudies",
+                                                "additionalRequiredValidation": []},
+                                               {"refClassName": "uk.ac.ebi.subs.data.component.ProtocolUse",
+                                                "dataTypeIdForReferencedDocument": "functionalGenomicsProtocols",
+                                                "additionalRequiredValidation": []},
+                                               {"refClassName": "uk.ac.ebi.subs.data.component.SampleUse",
+                                                "dataTypeIdForReferencedDocument": "functionalGenomicsSamples",
+                                                "additionalRequiredValidation": []}],
+                                           "requiredValidationAuthors": ["core",
+                                                                         "JsonSchema"],
+                                           "optionalValidationAuthors": [],
+                                           "submittableClassName": "uk.ac.ebi.subs.repository.model.Assay",
+                                           "archive": "ArrayExpress"},
     "functionalGenomicsAssayData": {},
     "functionalGenomicsAnalysis": {},
     "functionalGenomicsFiles": {}
@@ -65,21 +141,17 @@ db_url = environ.get("DBCON")
 
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 client = MongoClient(db_url)
-db = client.submissions_dev
+db = getattr(client, environ.get("SUBS_DATABASE"))
 
 
-files_entry = db.dataType.find({"_id": "files"})
+files_entry = getattr(db, environ.get("DATA_TYPE_COLLECTION")).find_one({"_id": "files"})
 
 pprint(files_entry)
-# Issue the serverStatus command and print the results
-#serverStatusResult = db.command("serverStatus")
-#pprint(serverStatusResult)
-
-
 
 
 
 
 if __name__ == "__main__":
-    print(load_schema_for_mongo("arrayexpress_sample_schema.json"))
-    print(re.sub(r"\$", "#dollar#", "{$schema: test string}"))
+    #print(load_schema_for_mongo("arrayexpress_sample_schema.json"))
+    #print(re.sub(r"\$", "#dollar#", "{$schema: test string}"))
+    pass
